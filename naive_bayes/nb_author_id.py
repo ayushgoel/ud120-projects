01203b9dbaa797a9f84418bdf919ea711a75ef20
Ayush Goel <ayushgoel111@gmail.com>
2017-08-14 18:43:28 +0530

    Add code to print accuracy of predictions

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -21,13 +21,12 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
 
+# predicted = clf.predict(labels_train)
 
+accuracy = clf.score(features_test, labels_test)
 
-#########################################################
-### your code goes here ###
-
-
-#########################################################
-
-
+print "Accuracy: {0}".format(accuracy)
